e9a1f4d40499e13369038e6295c742c84547d1a1
jyosthna1 <jyosthna1>
2023-11-21 14:30:51 +0000

    new211

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,8 +2,18 @@ import './index.css'
 import Header from '../Header'
 
 const Home = () => (
-  <div>
+  <div className="home-container">
     <Header />
+    <div className="home-page-details">
+      <h1 className="head">Find The Job That Fits Your Life</h1>
+      <p className="description">
+        Millions of people are searching for jobs,salary information,company
+        reviews.Find the job that fits your ability and potential.
+      </p>
+      <button type="button" className="findJobsButton">
+        Find Jobs
+      </button>
+    </div>
   </div>
 )
 
